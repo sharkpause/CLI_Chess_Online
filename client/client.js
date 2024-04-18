ff46368f85bb765c9db962b99fe0838b30081b82
Gilbert Donell Chen <carlmabfredishere@gmail.com>
2024-04-18 15:09:35 +0700

    Moved loginUI function to its own file

--- client/client.js
+++ client/client.js
@@ -40,31 +40,6 @@ async function mainMenuUI() {
 	else if(mainMenuAnswer === 'login') loginUI();
 }
 
-async function loginUI() {
-	let errorMessage = '';
-
-	while(true) {
-		console.clear();
-		console.log(chalk.cyanBright('Log into your CLI Chess Online account!\n'));
-
-		if(errorMessage !== '') console.log(chalk.red(errorMessage), '\n');
-
-		const usernameInput = await input({
-			message: 'Username: '
-		});
-		const passwordInput = await password({
-			message: 'Password: '
-		});
-	
-		
-		if(!/^[a-zA-Z0-9_]+$/.test(usernameInput) || usernameInput > 30 || usernameInput < 3) {
-			errorMessage = 'Username must only be composed of alphanumeric characters and underscores and be shorter than 30 characters and longer than 3 characters';
-		} else {
-			console.log(chalk.cyan('\nLogging in...Please wait patiently'));
-		}
-	}
-}
-
 mainMenuUI();
 
 //hide();
