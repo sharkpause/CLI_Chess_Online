f35d7d37004bc1c083cfd327af81214a97b483b4
Gilbert Donell Chen <carlmabfredishere@gmail.com>
2024-05-18 16:43:03 +0700

    Started working on bishop moves and delete the validate function to just move the code in the main function

--- client/pages/chess_utils/generateMoves.js
+++ client/pages/chess_utils/generateMoves.js
@@ -6,14 +6,26 @@ export default function generateMoves(square, board) {
 			return generateWhitePawnMoves(square, board);
 		case 2:
 			return generateWhiteKnightMoves(square, board);
+		case 3:
+			return generateWhiteBishopMoves(square, board);
 	}
+}
+
+function generateWhiteBishopMoves(square, board) {
+	const moves = [];
+
+	for(let i = 0; i < board.length; ++i) {
+		;
+	}
+
+	return moves;
 }
 
 function generateWhitePawnMoves(square, board) {
 	const moves = [];
 
 	if(COORDINATES[square[1]] - 2 > 0 && board[COORDINATES[square[1]] - 2][COORDINATES[square[0]]] === 0) {
-		// Checks if two squares in front of the pawn is empty
+		// Checks if two squares in front of the pawn is empty and not outside the board
 		moves.push(square[0] + RANK_COORDINATES[COORDINATES[square[1]] - 2]);
 	}
 	if(COORDINATES[square[1]] - 1 > 0 && board[COORDINATES[square[1]] - 1][COORDINATES[square[0]]] === 0) {
--- client/pages/chess_utils/validateMove.js
+++ client/pages/chess_utils/validateMove.js
@@ -4,18 +4,7 @@ import generateMoves from './generateMoves.js';
 export default function validateMove(before, after, board) {
 	const movingPiece = board[COORDINATES[before[1]]][COORDINATES[before[0]]];
 
-	switch(movingPiece) {
-		case 1:
-			return validate(before, after, board);
-		case 2:
-			return validate(before, after, board);
-	}
-}
-
-function validate(before, after, board) {
 	const moves = generateMoves(before, board);
-
-	console.log(moves);
 
 	if(moves.indexOf(after) > -1) return true;
 	return false;
