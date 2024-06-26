456ce7d5320d362096cac6c111b1f4ba6aa6af63
Gilbert Donell Chen <carlmabfredishere@gmail.com>
2024-06-27 00:38:43 +0700

    Added a new function for generating moves for inCheck exclusively

---
+++ client/pages/chess_utils/generateMovesForChecks.js
@@ -0,0 +1,270 @@
+import { COORDINATES, FILE_COORDINATES, RANK_COORDINATES } from './constants.js';
+import isEnemyPiece from './isEnemyPiece.js';
+
+export default function generateMoves(square, board) {
+	switch(board[COORDINATES[square[1]]][COORDINATES[square[0]]]) {
+		case 1:
+			return generateWhitePawnMoves(square, board);
+		case 2:
+			return generateKnightMoves(square, board, 1); // 0: White, 1: Black
+		case 3:
+			return generateBishopMoves(square, board, 1);
+		case 4:
+			return generateRookMoves(square, board, 1);
+		case 5:
+			const diagonalsWhite = generateBishopMoves(square, board, 1);
+			const straightsWhite = generateRookMoves(square, board, 1);
+
+			return diagonalsWhite + straightsWhite;
+		case 6:
+			return generateKingMoves(square, board, 1);
+		case 8:
+			return generateBlackPawnMoves(square, board);
+		case 9:
+			return generateKnightMoves(square, board, 0);
+		case 10:
+			return generateBishopMoves(square, board, 0);
+		case 11:
+			return generateRookMoves(square, board, 0);
+		case 12:
+			const diagonalsBlack = generateBishopMoves(square, board, 0);
+			const straightsBlack = generateRookMoves(square, board, 0);
+
+			return diagonalsBlack + straightsBlack;
+		case 13:
+			return generateKingMoves(square, board, 0);
+	}
+}
+
+function generateKingMoves(square, board, enemyColor) {
+	const moves = [];
+
+	if(COORDINATES[square[0]]-1 >= 0 && (board[COORDINATES[square[1]]][COORDINATES[square[0]]-1] === 0 || isEnemyPiece(board[COORDINATES[square[1]]][COORDINATES[square[0]]-1], enemyColor))) {
+		moves.push(FILE_COORDINATES[COORDINATES[square[0]]-1] + square[1]); // Left
+	}
+	if(COORDINATES[square[0]]+1 < 8 && (board[COORDINATES[square[1]]][COORDINATES[square[0]]+1] === 0 || isEnemyPiece(board[COORDINATES[square[1]]][COORDINATES[square[0]]+1], enemyColor))) {
+		moves.push(FILE_COORDINATES[COORDINATES[square[0]]+1] + square[1]); // Right
+	}
+	if(COORDINATES[square[1]]-1 >= 0 && (board[COORDINATES[square[1]]-1][COORDINATES[square[0]]] === 0 || isEnemyPiece(board[COORDINATES[square[1]]-1][COORDINATES[square[0]]], enemyColor))) {
+		moves.push(square[0] + RANK_COORDINATES[COORDINATES[square[1]]-1]); // Top
+	}
+	if(COORDINATES[square[1]]+1 < 8 && (board[COORDINATES[square[1]]+1][COORDINATES[square[0]]] === 0 || isEnemyPiece(board[COORDINATES[square[1]]+1][COORDINATES[square[0]]], enemyColor))) {
+		moves.push(square[0] + RANK_COORDINATES[COORDINATES[square[1]]+1]); // Bottom
+	}
+
+	if((COORDINATES[square[0]]-1 >= 0 && COORDINATES[square[1]]-1 >= 0) && (board[COORDINATES[square[1]]-1][COORDINATES[square[0]]-1] === 0 || isEnemyPiece(board[COORDINATES[square[1]]-1][COORDINATES[square[0]]-1], enemyColor))) {
+		moves.push(FILE_COORDINATES[COORDINATES[square[0]]-1] + RANK_COORDINATES[COORDINATES[square[1]]-1]); // Top-Left
+	}
+	if((COORDINATES[square[0]]+1 < 8 && COORDINATES[square[1]]-1 >= 0) && (board[COORDINATES[square[1]]-1][COORDINATES[square[0]]+1] === 0 || isEnemyPiece(board[COORDINATES[square[1]]-1][COORDINATES[square[0]]+1], enemyColor))) {
+		moves.push(FILE_COORDINATES[COORDINATES[square[0]]+1] + RANK_COORDINATES[COORDINATES[square[1]]-1]); // Top-Right
+	}
+	if((COORDINATES[square[0]]-1 >= 0 && COORDINATES[square[1]]+1 < 8) && (board[COORDINATES[square[1]]+1][COORDINATES[square[0]]-1] === 0 || isEnemyPiece(board[COORDINATES[square[1]]+1][COORDINATES[square[0]]-1], enemyColor))) {
+		moves.push(FILE_COORDINATES[COORDINATES[square[0]]-1] + RANK_COORDINATES[COORDINATES[square[1]]+1]); // Bottom-Left
+	}
+	if((COORDINATES[square[0]]+1 < 8 && COORDINATES[square[1]]+1 < 8) && (board[COORDINATES[square[1]]+1][COORDINATES[square[0]]+1] === 0 || isEnemyPiece(board[COORDINATES[square[1]]+1][COORDINATES[square[0]]+1], enemyColor))) {
+		moves.push(FILE_COORDINATES[COORDINATES[square[0]]+1] + RANK_COORDINATES[COORDINATES[square[1]]+1]); // Bottom-Right
+	}
+
+	return moves;
+}
+
+function generateRookMoves(square, board, enemyColor, kingPosition) {
+	const moves = [];
+
+	for(let i = 1; i < board.length; ++i) { // Left, condition: 0 = checks for empty
+		if(COORDINATES[square[0]]-i >= 0) {
+			if(board[COORDINATES[square[1]]][COORDINATES[square[0]]-i] === 0) {
+				moves.push(FILE_COORDINATES[COORDINATES[square[0]]-i] + square[1]);
+			} else if(isEnemyPiece(board[COORDINATES[square[1]]][COORDINATES[square[0]]-i], enemyColor)) {
+				moves.push(FILE_COORDINATES[COORDINATES[square[0]]-i] + square[1]);
+				break;
+			} else {
+				break;
+			}
+		}
+	}
+
+	for(let i = 1; i < board.length; ++i) { // Top
+		if(COORDINATES[square[1]]-i >= 0) {
+			if(board[COORDINATES[square[1]]-i][COORDINATES[square[0]]] === 0) {
+				moves.push(square[0] + RANK_COORDINATES[COORDINATES[square[1]]-i]);
+			} else if(isEnemyPiece(board[COORDINATES[square[1]]-i][COORDINATES[square[0]]], enemyColor)) {
+				moves.push(square[0] + RANK_COORDINATES[COORDINATES[square[1]]-i]);
+				break;
+			} else {
+				break;
+			}
+		}
+	}
+
+	for(let i = 1; i < board.length; ++i) { // Right
+		if(COORDINATES[square[0]]+i < 8) {
+			if(board[COORDINATES[square[1]]][COORDINATES[square[0]]+i] === 0) {
+				moves.push(FILE_COORDINATES[COORDINATES[square[0]]+i] + square[1]);
+			} else if(isEnemyPiece(board[COORDINATES[square[1]]][COORDINATES[square[0]]+i], enemyColor)) {
+				moves.push(FILE_COORDINATES[COORDINATES[square[0]]+i] + square[1]);
+				break;
+			} else {
+				break;
+			}
+		}
+	}
+
+	for(let i = 1; i < board.length; ++i) { // Bottom
+		if(COORDINATES[square[1]]+i < 8) {
+			if(board[COORDINATES[square[1]]+i][COORDINATES[square[0]]] === 0) {
+				moves.push(square[0] + RANK_COORDINATES[COORDINATES[square[1]]+i]);
+			} else if(isEnemyPiece(board[COORDINATES[square[1]]+i][COORDINATES[square[0]]], enemyColor)) {
+				moves.push(square[0] + RANK_COORDINATES[COORDINATES[square[1]]+i]);
+				break;
+			} else {
+				break;
+			}
+		}
+	}
+
+	return moves;
+}
+
+function generateBishopMoves(square, board, enemyColor, kingPosition) {
+	const moves = [];
+
+	for(let i = 1; i < board.length; ++i) { // Top-left
+		if(COORDINATES[square[0]]-i >= 0 && COORDINATES[square[1]]-i >= 0) {
+			if(board[COORDINATES[square[1]]-i][COORDINATES[square[0]]-i] === 0) {
+				moves.push(FILE_COORDINATES[COORDINATES[square[0]]-i] + RANK_COORDINATES[COORDINATES[square[1]]-i]);
+			} else if(isEnemyPiece(board[COORDINATES[square[1]]-i][COORDINATES[square[0]]-i], enemyColor)) {
+				moves.push(FILE_COORDINATES[COORDINATES[square[0]]-i] + RANK_COORDINATES[COORDINATES[square[1]]-i]);
+				break;
+			} else {
+				break;
+			}
+		}
+	}
+
+	for(let i = 1; i < board.length; ++i) { // Bottom-right
+		if(COORDINATES[square[0]]+i < 8 && COORDINATES[square[1]]+i < 8) {
+			if(board[COORDINATES[square[1]]+i][COORDINATES[square[0]]+i] === 0) {
+				moves.push(FILE_COORDINATES[COORDINATES[square[0]]+i] + RANK_COORDINATES[COORDINATES[square[1]]+i]);
+			} else if(isEnemyPiece(board[COORDINATES[square[1]]+i][COORDINATES[square[0]]+i], enemyColor)) {
+				moves.push(FILE_COORDINATES[COORDINATES[square[0]]+i] + RANK_COORDINATES[COORDINATES[square[1]]+i]);
+				break;
+			} else {
+				break;
+			}
+		}
+	}
+
+	for(let i = 1; i < board.length; ++i) { // Top-right
+		if(COORDINATES[square[0]]-i >= 0 && COORDINATES[square[1]]+i < 8) {
+			if(board[COORDINATES[square[1]]+i][COORDINATES[square[0]]-i] === 0) {
+				moves.push(FILE_COORDINATES[COORDINATES[square[0]]-i] + RANK_COORDINATES[COORDINATES[square[1]]+i]);
+			} else if(isEnemyPiece(board[COORDINATES[square[1]]+i][COORDINATES[square[0]]-i], enemyColor)) {
+				moves.push(FILE_COORDINATES[COORDINATES[square[0]]-i] + RANK_COORDINATES[COORDINATES[square[1]]+i]);
+				break;
+			} else {
+				break;
+			}
+		}
+	}
+
+	for(let i = 1; i < board.length; ++i) { // Bottom-left
+		if(COORDINATES[square[0]]+i < 8 && COORDINATES[square[1]]-i >= 0) {
+			if(board[COORDINATES[square[1]]-i][COORDINATES[square[0]]+i] === 0) {
+				moves.push(FILE_COORDINATES[COORDINATES[square[0]]+i] + RANK_COORDINATES[COORDINATES[square[1]]-i]);
+			} else if(isEnemyPiece(board[COORDINATES[square[1]]-i][COORDINATES[square[0]]+i], enemyColor)) {
+				moves.push(FILE_COORDINATES[COORDINATES[square[0]]+i] + RANK_COORDINATES[COORDINATES[square[1]]-i]);
+				break;
+			} else {
+				break;
+			}
+		}
+	}	
+
+	return moves;
+}
+
+function generateWhitePawnMoves(square, board, kingPosition) {
+	const moves = [];
+
+	if(COORDINATES[square[1]]-2 > 0 && board[COORDINATES[square[1]]-2][COORDINATES[square[0]]] === 0) {
+		// Checks if two squares in front of the pawn is empty and not outside the board
+		moves.push(square[0] + RANK_COORDINATES[COORDINATES[square[1]]-2]);
+	}
+	if(COORDINATES[square[1]]-1 > 0 && board[COORDINATES[square[1]]-1][COORDINATES[square[0]]] === 0) {
+		// Checks if one square in front of the pawn is empty
+		moves.push(square[0] + RANK_COORDINATES[COORDINATES[square[1]]-1]);
+	}
+	if(COORDINATES[square[1]]-1 > 0 && board[COORDINATES[square[1]]-1][COORDINATES[square[0]]-1] > 7) {
+		// Checks if one square in front of the pawn and to the left has an enemy piece
+		moves.push(FILE_COORDINATES[COORDINATES[square[0]]-1] + RANK_COORDINATES[COORDINATES[square[1]]-1]);
+	}
+	if(COORDINATES[square[1]]-1 > 0 && board[COORDINATES[square[1]]-1][COORDINATES[square[0]]+1] > 7) {
+		// Checks if one square in front of the pawn and to the right has an enemy piece
+		moves.push(FILE_COORDINATES[COORDINATES[square[0]]+1] + RANK_COORDINATES[COORDINATES[square[1]]-1]);
+	}
+
+	return moves;
+}
+
+function generateBlackPawnMoves(square, board, kingPosition) {
+	const moves = [];
+
+	if(COORDINATES[square[1]]+2 > 0 && board[COORDINATES[square[1]]+2][COORDINATES[square[0]]] === 0) {
+		// Checks if two squares in front of the pawn is empty and not outside the board
+		moves.push(square[0] + RANK_COORDINATES[COORDINATES[square[1]]+2]);
+	}
+	if(COORDINATES[square[1]]+1 > 0 && board[COORDINATES[square[1]]+1][COORDINATES[square[0]]] === 0) {
+		// Checks if one square in front of the pawn is empty
+		moves.push(square[0] + RANK_COORDINATES[COORDINATES[square[1]]+1]);
+	}
+	if(COORDINATES[square[1]]+1 > 0 && board[COORDINATES[square[1]]+1][COORDINATES[square[0]]-1] < 8) {
+		// Checks if one square in front of the pawn and to the left has an enemy piece
+		moves.push(FILE_COORDINATES[COORDINATES[square[0]]-1] + RANK_COORDINATES[COORDINATES[square[1]]+1]);
+	}
+	if(COORDINATES[square[1]]+1 > 0 && board[COORDINATES[square[1]]+1][COORDINATES[square[0]]+1] < 8) {
+		// Checks if one square in front of the pawn and to the right has an enemy piece
+		moves.push(FILE_COORDINATES[COORDINATES[square[0]]+1] + RANK_COORDINATES[COORDINATES[square[1]]+1]);
+	}
+
+	return moves;
+}
+
+function generateKnightMoves(square, board, enemyColor, kingPosition) {
+	const moves = [];
+
+	if((COORDINATES[square[1]] - 2 >= 0 && COORDINATES[square[0]] - 1 >= 0) && (board[COORDINATES[square[1]] - 2][COORDINATES[square[0]] - 1] === 0 || isEnemyPiece(board[COORDINATES[square[1]] - 2][COORDINATES[square[0]] - 1] > 7, enemyColor))) {
+		// Check if two squares up and one square left is empty
+		moves.push(FILE_COORDINATES[COORDINATES[square[0]] - 1] + RANK_COORDINATES[COORDINATES[square[1]] - 2]);
+	}
+	if((COORDINATES[square[1]] - 2 >= 0 && COORDINATES[square[0]] + 1 < 8) && (board[COORDINATES[square[1]] - 2][COORDINATES[square[0]] + 1] === 0 || isEnemyPiece(board[COORDINATES[square[1]] - 2][COORDINATES[square[0]] + 1] > 7, enemyColor))) {
+		// Check if two squares up and one square right is empty
+		moves.push(FILE_COORDINATES[COORDINATES[square[0]] + 1] + RANK_COORDINATES[COORDINATES[square[1]] - 2]);
+	}
+	if((COORDINATES[square[1]] + 2 < 8 && COORDINATES[square[0]] - 1 >= 0) && (board[COORDINATES[square[1]] + 2][COORDINATES[square[0]] - 1] === 0 || isEnemyPiece(board[COORDINATES[square[1]] + 2][COORDINATES[square[0]] - 1] > 7, enemyColor))) {
+		// Check if two squares down and one square left is empty
+		moves.push(FILE_COORDINATES[COORDINATES[square[0]] - 1] + RANK_COORDINATES[COORDINATES[square[1]] + 2]);
+	}
+	if((COORDINATES[square[1]] + 2 < 8 && COORDINATES[square[0]] + 1 < 8) && (board[COORDINATES[square[1]] + 2][COORDINATES[square[0]] + 1] === 0 || isEnemyPiece(board[COORDINATES[square[1]] + 2][COORDINATES[square[0]] + 1] > 7, enemyColor))) {
+		// Check if two squares down and one square right is empty
+		moves.push(FILE_COORDINATES[COORDINATES[square[0]] + 1] + RANK_COORDINATES[COORDINATES[square[1]] + 2]);
+	}
+	if((COORDINATES[square[1]] - 1 >= 0 && COORDINATES[square[0]] - 2 >= 0) && (board[COORDINATES[square[1]] - 1][COORDINATES[square[0]] - 2] === 0 || isEnemyPiece(board[COORDINATES[square[1]] - 1][COORDINATES[square[0]] - 2] > 7, enemyColor))) {
+		// Check if two squares left and one square left is empty
+		moves.push(FILE_COORDINATES[COORDINATES[square[0]] - 2] + RANK_COORDINATES[COORDINATES[square[1]] - 1]);
+	}
+	if((COORDINATES[square[1]] + 1 < 8 && COORDINATES[square[0]] - 2 >= 0) && (board[COORDINATES[square[1]] + 1][COORDINATES[square[0]] - 2] === 0 || isEnemyPiece(board[COORDINATES[square[1]] + 1][COORDINATES[square[0]] - 2] > 7, enemyColor))) {
+		// Check if two squares left and one square right is empty
+		moves.push(FILE_COORDINATES[COORDINATES[square[0]] - 2] + RANK_COORDINATES[COORDINATES[square[1]] + 1]);
+	}
+	if((COORDINATES[square[1]] - 1 >= 0 && COORDINATES[square[0]] + 2 < 8) && (board[COORDINATES[square[1]] - 1][COORDINATES[square[0]] + 2] === 0 || isEnemyPiece(board[COORDINATES[square[1]] - 1][COORDINATES[square[0]] + 2] > 7, enemyColor))) {
+		// Check if two squares right and one square left is empty
+		moves.push(FILE_COORDINATES[COORDINATES[square[0]] + 2] + RANK_COORDINATES[COORDINATES[square[1]] - 1]);
+	}
+	if((COORDINATES[square[1]] + 1 < 8 && COORDINATES[square[0]] + 2 < 8) && (board[COORDINATES[square[1]] + 1][COORDINATES[square[0]] + 2] === 0 || isEnemyPiece(board[COORDINATES[square[1]] + 1][COORDINATES[square[0]] + 2], enemyColor))) {
+		// Check if two squares right and one square right is empty
+		moves.push(FILE_COORDINATES[COORDINATES[square[0]] + 2] + RANK_COORDINATES[COORDINATES[square[1]] + 1]);
+	}
+
+	return moves;
+}
--- client/pages/chess_utils/inCheck.js
+++ client/pages/chess_utils/inCheck.js
@@ -1,4 +1,4 @@
-import generateMoves from './generateMoves.js';
+import generateMovesForChecks from './generateMovesForChecks.js';
 import isEnemyPiece from './isEnemyPiece.js';
 
 import { FILE_COORDINATES, RANK_COORDINATES } from './constants.js';
@@ -7,7 +7,7 @@ export default function inCheck(kingSquare, board, enemyColor) {
 	for(let i = 0; i < board.length; ++i) {
 		for(let j = 0; j < board[i].length; ++j) {
 			if(board[i][j] !== 0 && isEnemyPiece(board[i][j], enemyColor)) {
-				const moves = generateMoves(FILE_COORDINATES[j]+RANK_COORDINATES[i], board);
+				const moves = generateMovesForChecks(FILE_COORDINATES[j]+RANK_COORDINATES[i], board);
 				if(typeof moves !== 'undefined' && moves.indexOf(kingSquare) > -1) return true;
 			}
 		}
